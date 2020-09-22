19f0c93385714fc0cb35582dbd5badecc93586ef
Wilinton Vidal <wjaniervalvarez@misena.edu.co>
2020-09-21 21:20:52 -0500

    Proyecto realizado con svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,13 @@
+<div class="Footer">
+    <div class="Footer-container">
+        <div class="Footer-copy">2020 pugstagram por Wilinton Vidal</div>
+    </div>
+</div>
+<style>
+    .Footer {
+      font-size: 11px;
+      letter-spacing: 1px;
+      font-weight: normal;
+      color: #c7c7c7;
+    }
+  </style>
---
+++ src/components/TimeLine.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Card from './Card.svelte';
+    export let posts = [];
+</script>
+
+<div class="TimeLine">
+    <div class="TimeLine-container">
+      {#each posts as post}
+        <Card {...post}/>
+      {:else}
+        <p>Loading...</p>
+      {/each}
+        
+    </div>
+</div>
+
+  <!-- TimeLine.svelte -->
+  
+  
+  <style>
+    .TimeLine {
+      padding: 4em 0 0 0;
+    }
+  </style>
